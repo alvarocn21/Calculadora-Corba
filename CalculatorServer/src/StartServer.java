846fddb327ab8ef679fc32ae09519b7b1497b81d
alvarocn21 <54987452+alvarocn21@users.noreply.github.com>
2023-03-06 18:12:30 +0100

    Update StartServer.java

--- CalculatorServer/src/StartServer.java
+++ CalculatorServer/src/StartServer.java
@@ -1,114 +1,48 @@
-/*
- * Para cambiar este encabezado de licencia, elija Encabezados de licencia en Propiedades del proyecto.
-* Para cambiar este archivo de plantilla, elija Herramientas | Plantillas
- * y abra la plantilla en el editor.
- */
-
-import Calculator.*;
+import Calculator.Calc;
+import Calculator.CalcHelper;
 import org.omg.CosNaming.*;
 import org.omg.CORBA.*;
-import java.util.*;
+import org.omg.PortableServer.*;
+import org.omg.PortableServer.POA;
  
-public class StartClient {
-    private static Calc calcObj;
-    
-    /**
-     * @param args los argumentos de la línea de comandos
-     */
-    public static void main(String[] args) {
-        try {
-            // crear e inicializar el ORB
-	    ORB orb = ORB.init(args, null);
+public class StartServer {
+ 
+    public static void main(String args[]) {
+        try{
+            // Creando e inicializando el ORB
+            ORB orb = ORB.init(args, null);  
             
-            // Obtener el contexto de nomenclatura raíz
-	    org.omg.CORBA.Object objRef = orb.resolve_initial_references("NameService");
+            // Cogemos la referencia del RootPOA y activamos el POAManager
+	    POA rootpoa = POAHelper.narrow(orb.resolve_initial_references("RootPOA"));
+            rootpoa.the_POAManager().activate();
+ 
+            // Creamos el servidor y registramos con el, el ORB
+            CalcObject calcObj = new CalcObject();
+            calcObj.setORB(orb); 
+ 
+            // Cogemos el objeto de referencia desde el Servidor
+            org.omg.CORBA.Object ref = rootpoa.servant_to_reference(calcObj);
+            Calc href = CalcHelper.narrow(ref);
+ 
+            // Cogemos el nombre del root
+            // El NameService invoca the nombre del servidor
+            org.omg.CORBA.Object nsRef =  orb.resolve_initial_references("NameService");
             
-            // Utilice NamingContextExt en lugar de NamingContext. Esto es 
-            // parte del Servicio de nomenclatura interoperable. 
-	    NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
-            
-            // resolver la referencia de objeto en nomenclatura
-	    calcObj = (Calc) CalcHelper.narrow(ncRef.resolve_str("Calculator"));
-
-            while(true) {
-                // Pedir información y leerla
-                System.out.println("------------------------------------------");
-                System.out.println("Introduce los parametros en formato [operator][sp][operand1][sp][operand2]."
-                        + "\nPor ejemplo: + 1 2");
-                Scanner c=new Scanner(System.in);
-		String input = c.nextLine();
-                
-                // if the command is exit, request the server to shutdown
-                if (input.toLowerCase().equals("Salir")) {
-                    calcObj.exit();
-                    break;
-                }
-                
-                // test the input
-                String[] inputParams = input.split(" ");
-                if (inputParams.length != 3) {
-                    System.out.println("Excepceion del cliente: Numero de parametro errorneo. Prueba de nuevo...");
-                    continue;
-                }
-                int operatorCode;
-                int operand1;
-                int operand2;
-                
-                // set calculation type
-                if (inputParams[0].equals("+")) {
-                    operatorCode = 1;
-                }
-                else if (inputParams[0].equals("-")) {
-                    operatorCode = 2;
-                }
-                else if (inputParams[0].equals("*")) {
-                    operatorCode = 3;
-                }
-                else if (inputParams[0].equals("/")) {
-                    operatorCode = 5;
-                }
-                else if (inputParams[0].equals("R")) {
-                    operatorCode = 4;
-                }
-                else {
-                    System.out.println("Excepceion del cliente: Operacion no reconocida. Prueba de nuevo...");
-                    continue;
-                }
-                
-                 //    Los operandos de entrada de prueba son enteros
-                try {
-                    operand1 = Integer.parseInt(inputParams[1]);
-                    operand2 = Integer.parseInt(inputParams[2]);
-                }
-                catch (NumberFormatException e) {
-                    System.out.println("Excepceion del cliente: Formato de numeros erroneos. Prueba de nuevo...");
-                    continue;
-                }
-                
-                
-                if (operatorCode == 4 && operand2 == 0) {
-                    System.out.println("Excepceion del cliente: No se puede dividir entre 0. Prueba de nuevo...");
-                    continue;
-                }
-                
-                
-		int result = calcObj.calculate(operatorCode, operand1, operand2);
-                String resultDisplay = "";
-                if (result == Integer.MAX_VALUE) {
-                    resultDisplay = "Excepceion del cliente: Tiene que ser un interger overflow. Prueba de nuevo...";
-                }
-                else if (result == Integer.MIN_VALUE) {
-                    resultDisplay = "Excepceion del cliente: Tiene que ser un integer underflow. Prueba de nuevo...";
-                }
-                else {
-                    resultDisplay = String.valueOf(result);
-                }
-		System.out.println("The result is: " + resultDisplay);
-            }
-        }
+            NamingContextExt ncRef = NamingContextExtHelper.narrow(nsRef);
+ 
+            // Unimos el nombre del objeto de refenciabind the Object Reference in Naming
+            NameComponent path[] = ncRef.to_name("Calculator");
+            ncRef.rebind(path, href);
+ 
+            System.out.println("CalculatorServer esta escuchando...");
+ 
+            // Espando a la invocacion del cliente
+            orb.run();
+            System.out.println("Etamos fuera");
+        } 
         catch (Exception e) {
-            System.out.println("Client exception: " + e.getMessage());
-            e.printStackTrace();
+            System.err.println("Server Error: " + e.getMessage());
+            e.printStackTrace(System.out);
         }
     }
 }
